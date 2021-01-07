88044d02402dbc5358e147f74e174cb529e11bf6
ajlee21 <alexjlee.21@gmail.com>
2021-01-07 16:06:42 -0500

    update comments and clean notebook

--- test_phage_reference/nbconverted/3_validate_expression_data.py
+++ test_phage_reference/nbconverted/3_validate_expression_data.py
@@ -50,37 +50,73 @@ expression_pa14_df.head()
 print("Total number of genes:", len(expression_phage_df.loc["SRR13234437"]))
 print("Percent of 0 expressed genes:",
       (expression_phage_df.loc["SRR13234437"] == 0).sum()/len(expression_phage_df.loc["SRR13234437"]))
-print(expression_phage_df.loc["SRR13234437"].max())
-sns.distplot(expression_phage_df.loc["SRR13234437"], kde=False)
 
 
-# In[7]:
+# In[27]:
+
+
+EColi_phage_ref_series = expression_phage_df.loc["SRR13234437"]
+nonzero_genes_EColi_phage_ref = EColi_phage_ref_series.iloc[EColi_phage_ref_series.nonzero()]
+print(nonzero_genes_EColi_phage_ref)
+
+
+# In[42]:
 
 
 # Check there is no expression in E. Coli sample using PAO1 reference
 print("Total number of genes:", len(expression_pao1_df.loc["SRR13234437"]))
 print("Percent of 0 expressed genes:", 
       (expression_pao1_df.loc["SRR13234437"] == 0).sum()/len(expression_pao1_df.loc["SRR13234437"]))
-print(expression_pao1_df.loc["SRR13234437"].max())
-sns.distplot(expression_pao1_df.loc["SRR13234437"], kde=False)
+
+print("Percent of 0 expressed genes:", 
+      ((expression_pao1_df.loc["SRR13234437"] > 0) & (expression_pao1_df.loc["SRR13234437"] < 1000))
+       .sum()/len(expression_pao1_df.loc["SRR13234437"]))
 
 
-# In[ ]:
+# In[48]:
+
+
+EColi_pao1_ref_series = expression_pao1_df.loc["SRR13234437"]
+nonzero_genes_EColi_pao1_ref = EColi_pao1_ref_series.iloc[EColi_pao1_ref_series.nonzero()]
+sns.distplot(nonzero_genes_EColi_pao1_ref, kde=False)
+
+# Get gene with the max expression
+EColi_pao1_ref_series[EColi_pao1_ref_series > 290000]
+
+
+# In[41]:
 
 
 # Check there is no expression in E. Coli sample using PA14 reference
 print("Total number of genes:", len(expression_pa14_df.loc["SRR13234437"]))
 print("Percent of 0 expressed genes:", 
       (expression_pa14_df.loc["SRR13234437"] == 0).sum()/len(expression_pa14_df.loc["SRR13234437"]))
-print(expression_pa14_df.loc["SRR13234437"].max())
-sns.distplot(expression_pa14_df.loc["SRR13234437"], kde=False)
 
+print("Percent of 0 expressed genes:", 
+      ((expression_pa14_df.loc["SRR13234437"] > 0) & (expression_pa14_df.loc["SRR13234437"] < 1000))
+       .sum()/len(expression_pa14_df.loc["SRR13234437"]))
+
+
+# In[47]:
+
+
+EColi_pa14_ref_series = expression_pa14_df.loc["SRR13234437"]
+nonzero_genes_EColi_pa14_ref = EColi_pa14_ref_series.iloc[EColi_pa14_ref_series.nonzero()]
+sns.distplot(nonzero_genes_EColi_pa14_ref, kde=False)
+
+# Get gene with the max expression
+EColi_pa14_ref_series[EColi_pa14_ref_series > 290000]
+
+
+# **Takeaway:**
+# 
+# * Most genes were found to have 0 expression, as expected
+# * Looks like the outlier gene encodes the elongation Tu which appears to be conserved across prokaryotes: https://pubmed.ncbi.nlm.nih.gov/6985898/
 
 # ## Positive case: PAO1
 # 
 # * Check that PAO1 sample (SRR13160334) aligns well to PAO1 and phage reference. 
-# * Check that PAO1 sample has expression at LUZ19 (NC_010326.1 in phage genome) and Pf1 genes (PA0717-PA0726 PAO1 genome)
-# * PA0717-PA0726 = PGD104183, PGD104185, PGD104187, PGD104189, PGD104191, PGD104193, PGD104195, PGD104197, PGD104199, PGD104201
+# * Check that PAO1 sample has expression at LUZ19 (NC_010326.1 in phage genome)
 
 # In[10]:
 
@@ -89,51 +125,38 @@ sns.distplot(expression_pa14_df.loc["SRR13234437"], kde=False)
 print("Total number of genes:", len(expression_pao1_df.loc["SRR13160334"]))
 print("Percent of 0 expressed genes:", 
       (expression_pao1_df.loc["SRR13160334"] > 0).sum()/len(expression_pao1_df.loc["SRR13160334"]))
-print(expression_pao1_df.loc["SRR13160334"].max())
-sns.distplot(expression_pao1_df.loc["SRR13160334"])
+
+
+# In[58]:
+
+
+# Plot the expression of PAO1 nonzero genes
+pao1_sample_pao1_ref_series = expression_pao1_df.loc["SRR13160334"]
+nonzero_genes_pao1_sample_pao1_ref = pao1_sample_pao1_ref_series.iloc[pao1_sample_pao1_ref_series.nonzero()]
+sns.distplot(nonzero_genes_pao1_sample_pao1_ref, kde=False)
+
+# Get gene with the max expression
+
+len(pao1_sample_pao1_ref_series[(pao1_sample_pao1_ref_series < 1000) & pao1_sample_pao1_ref_series > 0])/len(pao1_sample_pao1_ref_series)
 
 
 # In[13]:
-
-
-"""# Check there is expression in PAO1 sample using PAO1 reference genes: PA0717-PA0726
-pao1_pf1s = [
-    "PGD104183",
-    "PGD104185",
-    "PGD104187",
-    "PGD104189",
-    "PGD104191",
-    "PGD104193", 
-    "PGD104195", 
-    "PGD104197",
-    "PGD104199", 
-    "PGD104201"
-    ]
-expression_pao1_df.loc["SRR13160334", pao1_pf1s]"""
-
-
-# In[11]:
-
-
-"""# Check there is expression in PAO1 sample using phage reference
-print("Total number of genes:", len(expression_phage_df.loc["SRR13160334"]))
-print("Percent of 0 expressed genes:", 
-      (expression_phage_df.loc["SRR13160334"] > 0).sum()/len(expression_phage_df.loc["SRR13160334"]))
-print(expression_phage_df.loc["SRR13160334"].max())
-sns.distplot(expression_phage_df.loc["SRR13160334"], kde=True)"""
-
-
-# In[12]:
 
 
 # Check there is expression in PAO1 sample using phage reference gene: NC_010326.1
 expression_phage_df.loc["SRR13160334", "NC_010326.1"]
 
 
+# **Takeaway:**
+# 
+# * Most genes in PAO1 sample are nonzero, including phage gene that the sample is known to contain -- as expected
+# * Is the TPM < 1000 expected?
+# * Why is TPM of phage gene higher than other PAO1 genes?
+
 # ## Positive case: PA14
 # 
 # * Check that PA14 sample (ERR3642743) aligns well to PA14 and phage references. 
-# * Check that PA14 sample has expression at Pf5 (PA14_49010, PA14_49020 in PA14 genome) and Pf1 genes (AY324828.1, NC_001331.1, MG250485.1 in phage genome)
+# * Check that PA14 sample has expression at Pf5 (PA14_49010, PA14_49020 in PA14 genome) 
 # * PA14_49010, PA14_49020 = PGD1658748, PGD1658750
 
 # In[14]:
@@ -143,8 +166,20 @@ expression_phage_df.loc["SRR13160334", "NC_010326.1"]
 print("Total number of genes:", len(expression_pa14_df.loc["ERR3642743"]))
 print("Percent of 0 expressed genes:", 
       (expression_pa14_df.loc["ERR3642743"] > 0).sum()/len(expression_pa14_df.loc["ERR3642743"]))
-print(expression_pa14_df.loc["ERR3642743"].max())
 sns.distplot(expression_pa14_df.loc["ERR3642743"], kde=False)
+
+
+# In[60]:
+
+
+# Plot the expression of PAO1 nonzero genes
+pa14_sample_pa14_ref_series = expression_pa14_df.loc["ERR3642743"]
+nonzero_genes_pa14_sample_pa14_ref = pa14_sample_pa14_ref_series.iloc[pa14_sample_pa14_ref_series.nonzero()]
+sns.distplot(nonzero_genes_pa14_sample_pa14_ref, kde=False)
+
+# Get gene with the max expression
+
+len(pa14_sample_pa14_ref_series[(pa14_sample_pa14_ref_series < 1000) & pa14_sample_pa14_ref_series > 0])/len(pa14_sample_pa14_ref_series)
 
 
 # In[15]:
@@ -154,27 +189,7 @@ sns.distplot(expression_pa14_df.loc["ERR3642743"], kde=False)
 expression_pa14_df.loc["ERR3642743", ["PGD1658748", "PGD1658750"]]
 
 
-# In[ ]:
-
-
-# Check there is expression in PA14 sample using phage reference
-#sns.distplot(expression_phage_df.loc["ERR3642743"], kde=False)
-
-
-# In[ ]:
-
-
-# Check there is expression in PA14 sample using phage reference genes: 
-#expression_phage_df.loc["ERR3642743", ["AY324828.1", "NC_001331.1", "MG250485.1"]]
-
-
-# **Initial Takeaways:**
+# **Takeaways:**
 # 
-# As expected:
-# * E. Coli samples had 0 expression using phage and PAO1 references
-# * PAO1 sample had expression using PAO1 reference
-# * Phage samples had 0 expression using PAO1 reference
-# 
-# Issues:
-# * Pseudomonas phage Epa14, Epa41 samples had 0 expression using phage reference → Not sure why
-# 
+# * Most genes in PA14 sample are nonzero, including phage gene that the sample is known to contain -- as expected
+# * Is the TPM < 1000 expected?
