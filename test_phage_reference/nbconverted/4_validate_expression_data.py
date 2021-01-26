c6925fc79362e1047358acc21b63cf6343cbadc8
ajlee21 <alexjlee.21@gmail.com>
2021-01-26 15:49:13 -0500

    update comments in nbs

--- test_phage_reference/nbconverted/4_validate_expression_data.py
+++ test_phage_reference/nbconverted/4_validate_expression_data.py
@@ -142,11 +142,19 @@ print(f"Percent of nonzero genes below {threshold}:",
 fig5 = sns.distplot(majority_genes_EColi_pa14_phage_ref, kde=False)
 
 
-# # TO DO:
+# In[15]:
+
+
+# Get outlier gene
+print(nonzero_genes_EColi_pao1_phage_ref[nonzero_genes_EColi_pao1_phage_ref == nonzero_genes_EColi_pao1_phage_ref.max()])
+print(nonzero_genes_EColi_pa14_phage_ref[nonzero_genes_EColi_pa14_phage_ref == nonzero_genes_EColi_pa14_phage_ref.max()])
+
+
 # **Takeaway:**
 # 
 # * Most genes were found to have 0 expression, as expected
-# * Looks like the outlier gene encodes the elongation Tu which appears to be conserved across prokaryotes: https://pubmed.ncbi.nlm.nih.gov/6985898/
+# * Looks like the outlier gene encodes tRNAs
+# the elongation Tu which appears to be conserved across prokaryotes: https://pubmed.ncbi.nlm.nih.gov/6985898/
 
 # ## Positive case: PAO1
 # 
@@ -154,7 +162,7 @@ fig5 = sns.distplot(majority_genes_EColi_pa14_phage_ref, kde=False)
 # * It aligns well to PAO1 reference. 
 # * It has expression at LUZ19 phage (NC_010326.1 in phage genome), since this sample was known to contain this phage gene.
 
-# In[15]:
+# In[16]:
 
 
 # Check there is expression in PAO1 sample using PAO1 reference
@@ -163,7 +171,7 @@ print("Percent of positive-expressed genes:",
       (expression_pao1_phage_df.loc["SRR13160334"] > 0).sum()/len(expression_pao1_phage_df.loc["SRR13160334"]))
 
 
-# In[16]:
+# In[17]:
 
 
 # Plot the expression of PAO1 nonzero genes
@@ -175,7 +183,7 @@ fig3 = sns.distplot(nonzero_genes_pao1_sample_pao1_phage_ref, kde=False)
 fig3.set_title("Expression of PAO1 sample mapped to PAO1+phage reference (TPM)")
 
 
-# In[17]:
+# In[18]:
 
 
 # If we remove the outlier gene, what does the expression look like
@@ -190,19 +198,25 @@ print(f"Percent of nonzero genes below {threshold}:",
 fig5 = sns.distplot(nonzero_genes_pao1_sample_pao1_phage_ref[nonzero_genes_pao1_sample_pao1_phage_ref < threshold], kde=False)
 
 
-# In[18]:
+# In[19]:
 
 
 # Check there is expression in PAO1 sample using phage reference gene: NC_010326.1
 expression_pao1_phage_df.loc["SRR13160334", "NC_010326.1"]
 
 
-# # TO DO
+# In[20]:
+
+
+# Look at outlier genes
+nonzero_genes_pao1_sample_pao1_phage_ref[nonzero_genes_pao1_sample_pao1_phage_ref > threshold]
+
+
 # **Takeaway:**
 # 
 # * Most genes in PAO1 sample are nonzero, including phage gene that the sample is known to contain -- as expected
-# * Is the TPM < 1000 expected?
-# * Why is TPM of phage gene higher than other PAO1 genes?
+# * 98% of the genes with nonzero expression have an expression level that is < 1000 TPM. Is this expected?
+# * Genes exceeding 1000 threshold are rRNAs
 
 # ## Positive case: PA14
 # 
@@ -213,7 +227,7 @@ expression_pao1_phage_df.loc["SRR13160334", "NC_010326.1"]
 # Implementation note:
 # * Here is the mapping from PA14 id to sequence id: PA14_49010, PA14_49020 = PGD1658748, PGD1658750
 
-# In[19]:
+# In[21]:
 
 
 # Check there is expression in PA14 sample using PA14 reference
@@ -222,7 +236,7 @@ print("Percent of positive-expressed genes:",
       (expression_pa14_phage_df.loc["ERR3642743"] > 0).sum()/len(expression_pa14_phage_df.loc["ERR3642743"]))
 
 
-# In[20]:
+# In[22]:
 
 
 # Plot the expression of PAO1 nonzero genes
@@ -237,7 +251,7 @@ fig4.set_title("Expression of PA14 sample mapped to PA14+phage reference (TPM)")
 #len(pa14_sample_pa14_ref_series[(pa14_sample_pa14_ref_series < 1000) & pa14_sample_pa14_ref_series > 0])/len(pa14_sample_pa14_ref_series)
 
 
-# In[21]:
+# In[23]:
 
 
 # If we remove the outlier gene, what does the expression look like
@@ -252,21 +266,24 @@ print(f"Percent of nonzero genes below {threshold}:",
 fig5 = sns.distplot(nonzero_genes_pa14_sample_pa14_phage_ref[nonzero_genes_pa14_sample_pa14_phage_ref < threshold], kde=False)
 
 
-# In[22]:
+# In[24]:
 
 
 # Check there is expression in PA14 sample using PA14 reference genes: PA14_49010, PA14_49020
 expression_pa14_phage_df.loc["ERR3642743", ["PGD1658748", "PGD1658750"]]
 
 
-# # TO DO:
-# 
-# Looks like there are outliers in all distributions. Why?
-# 
-# Does it make sense to remove those genes in the reference? Do the rest look as expected?
-# 
-# 
+# In[25]:
+
+
+# Look at outlier genes
+nonzero_genes_pa14_sample_pa14_phage_ref[nonzero_genes_pa14_sample_pa14_phage_ref > threshold]
+
+
 # **Takeaways:**
 # 
 # * Most genes in PA14 sample are nonzero, including phage gene that the sample is known to contain -- as expected
-# * Is the TPM < 1000 expected?
+# * 97% of the genes with nonzero expression have an expression level that is < 1000 TPM. Is this expected?
+# * Genes exceeding 1000 threshold are rRNAs, tRNAs
+# 
+# * Is this distribution shape, with a long right tail, expected?
