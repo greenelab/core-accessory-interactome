62c050ba2c5e8e42f6d9de54462286131b46f3e8
ajlee21 <alexjlee.21@gmail.com>
2021-06-03 16:33:11 -0400

    update validation scripts given new data

--- networks/3_module_validation.py
+++ networks/3_module_validation.py
@@ -6,11 +6,11 @@
 #       extension: .py
 #       format_name: light
 #       format_version: '1.5'
-#       jupytext_version: 1.9.1
+#       jupytext_version: 1.9.1+dev
 #   kernelspec:
-#     display_name: Python [conda env:core_acc_env] *
+#     display_name: Python [conda env:core_acc] *
 #     language: python
-#     name: conda-env-core_acc_env-py
+#     name: conda-env-core_acc-py
 # ---
 
 # ## Quick validation of network modules
@@ -24,28 +24,35 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
-from core_acc_modules import paths
+from core_acc_modules import utils, paths
 
 np.random.seed(1)
 
 # +
 # User params
+# Params to examine module size
+clustering_method_list = ["dbscan", "hierarchal", "affinity"]
+
+# Params for regulon/operon coverage
+# Clustering method to examine regulon/operon coverage
+method_toexamine = "dbscan"
+
 # Remove modules of this size or greater for analysis looking at coverage of regulon/operons
 module_size_threshold = 1000
-
-# Correlation threshold to determine which network modules to examine
-corr_threshold_toexamine = 0.9
 # -
 
 # ## Examine size of modules
+#
+# This will serve as a quick check that we are using reasonable clustering params in [2_get_network_communities.ipynb](2_get_network_communities.ipynb)
 
-# +
-corr_threshold_list = [0.9, 0.8, 0.7, 0.6, 0.5]
-
-for corr_threshold in corr_threshold_list:
-    print(f"Modules using correlation threshold: {corr_threshold}")
-    pao1_membership_filename = f"pao1_membership_{corr_threshold}.tsv"
-    pa14_membership_filename = f"pa14_membership_{corr_threshold}.tsv"
+for method_name in clustering_method_list:
+    print(f"Modules using correlation threshold: {method_name}")
+    pao1_membership_filename = os.path.join(
+        paths.LOCAL_DATA_DIR, f"pao1_modules_{method_name}.tsv"
+    )
+    pa14_membership_filename = os.path.join(
+        paths.LOCAL_DATA_DIR, f"pa14_modules_{method_name}.tsv"
+    )
 
     pao1_membership = pd.read_csv(
         pao1_membership_filename, sep="\t", header=0, index_col=0
@@ -58,18 +65,25 @@ for corr_threshold in corr_threshold_list:
     print(pa14_membership["module id"].value_counts())
 
 
+# +
+# Plot so that its one module id per bin -- step plot
+# Sort module ids when plotting, right now not sorted and averaging across
 # -
 
 # plotting function
-def plot_dist_modules(threshold_list):
+def plot_dist_modules(clustering_method_list):
 
     # Set up the matplotlib figure
-    fig, axes = plt.subplots(ncols=2, nrows=3, figsize=(15, 15))
+    fig, axes = plt.subplots(ncols=3, nrows=1, figsize=(15, 5))
     axes = axes.ravel()
 
-    for i in range(len(threshold_list)):
-        pao1_membership_filename = f"pao1_membership_{threshold_list[i]}.tsv"
-        pa14_membership_filename = f"pa14_membership_{threshold_list[i]}.tsv"
+    for i in range(len(clustering_method_list)):
+        pao1_membership_filename = os.path.join(
+            paths.LOCAL_DATA_DIR, f"pao1_modules_{clustering_method_list[i]}.tsv"
+        )
+        pa14_membership_filename = os.path.join(
+            paths.LOCAL_DATA_DIR, f"pa14_modules_{clustering_method_list[i]}.tsv"
+        )
 
         pao1_membership = pd.read_csv(
             pao1_membership_filename, sep="\t", header=0, index_col=0
@@ -102,7 +116,7 @@ def plot_dist_modules(threshold_list):
         )
 
         fig.set_title(
-            f"Histogram of size of modules using threshold {threshold_list[i]}",
+            f"Histogram of size of modules using {clustering_method_list[i]}",
             fontsize=12,
         )
         handles, labels = fig.get_legend_handles_labels()
@@ -110,15 +124,16 @@ def plot_dist_modules(threshold_list):
 
 
 # Plot distribution of modules per threshold
-plot_dist_modules(corr_threshold_list)
+plot_dist_modules(clustering_method_list)
 
 # **Takeaway:**
-# * Looks like as we decrease our correlation threshold (i.e. connections don't need to be very strong between nodes), more modules are able to form which is what I would expect.
-# * Genes in PA14 are less concentrated in one large module - there are many more small modules found.
+# * Looks like there is one large modules using DBSCAN and hierarachal clustering
+# * There are more even sized modules using affinity propogation
+# * For all methods, the size of the modules are fairly large (i.e. hundreds of genes in one modules)
 
 # ## Examine composition of modules
 #
-# We expect that genes within the same operon or regulon will cluster together (i.e. be within the same module). To test this we will compare the distribution of the number of modules that contain genes within the same regulon vs the number of modules that contain random genes
+# This is a negative control. We expect that genes within the same operon or regulon will cluster together (i.e. be within the same module). To test this we will compare the distribution of the number of modules that contain genes within the same regulon vs the number of modules that contain random genes
 #
 # _Some definitions:_
 #
@@ -132,12 +147,20 @@ pao1_regulon_filename = paths.PAO1_REGULON
 pao1_operon_filename = paths.PAO1_OPERON
 
 # Load membership for specific threshold
-pao1_membership_filename = f"pao1_membership_{corr_threshold_toexamine}.tsv"
+pao1_membership_filename = os.path.join(
+    paths.LOCAL_DATA_DIR, f"pao1_modules_{method_toexamine}.tsv"
+)
+pa14_membership_filename = os.path.join(
+    paths.LOCAL_DATA_DIR, f"pa14_modules_{method_toexamine}.tsv"
+)
 
 pao1_membership = pd.read_csv(pao1_membership_filename, sep="\t", header=0, index_col=0)
+pa14_membership = pd.read_csv(pa14_membership_filename, sep="\t", header=0, index_col=0)
 # -
 
 pao1_membership.head()
+
+pa14_membership.head()
 
 # According to Jake relationships tend to be more meaningful if the module is smaller (e.g. if an operon with 5 genes is contained in a module consisting of 10 total genes, this seems more biologically/functionally meaningful than an operon with 5 genes contained in a module consisting of 500 genes).
 #
@@ -251,6 +274,67 @@ pao1_operon.head()
 
 pao1_regulon.head()
 
+# ### Get operon/regulon information using PA14 ids
+
+pa14_operon = pao1_operon.copy()
+pa14_regulon = pao1_regulon.copy()
+
+# Get mapping between PAO1 and PA14 genes using PAO1 reference
+gene_annot_file = paths.GENE_PAO1_ANNOT
+gene_mapping_pao1 = utils.get_pao1_pa14_gene_map(gene_annot_file, "pao1")
+gene_mapping_pao1.head()
+
+pa14_operon["Genes_processed"] = pa14_operon["Genes_processed"].apply(
+    lambda pao1_gene_list: gene_mapping_pao1.loc[pao1_gene_list, "PA14_ID"].values
+)
+pa14_regulon["Genes_processed"] = pa14_regulon["Genes_processed"].apply(
+    lambda pao1_gene_list: gene_mapping_pao1.loc[pao1_gene_list, "PA14_ID"].values
+)
+
+# Update length based on filtered gene list ("Genes_processed" column)
+pa14_operon["Length_processed"] = pa14_operon["Genes_processed"].str.len()
+pa14_regulon["Length_processed"] = pa14_regulon["Genes_processed"].str.len()
+
+# +
+# If genes didn't map then drop operon/regulon
+pa14_operon = pa14_operon.drop(
+    pa14_operon[
+        pa14_operon["Genes_processed"].apply(lambda gene_list: pd.isna(gene_list).any())
+    ].index
+)
+pa14_regulon = pa14_regulon.drop(
+    pa14_regulon[
+        pa14_regulon["Genes_processed"].apply(
+            lambda gene_list: pd.isna(gene_list).any()
+        )
+    ].index
+)
+
+print(pa14_operon.shape)
+print(pa14_regulon.shape)
+
+# +
+# If number genes in operon are 1 then remove
+# Drop operons and regulons that have 0 genes due to no module filtering
+pa14_operon = pa14_operon.drop(pa14_operon.query("Length_processed<=1").index)
+pa14_regulon = pa14_regulon.drop(pa14_regulon.query("Length_processed<=1").index)
+
+print(pa14_operon.shape)
+print(pa14_regulon.shape)
+# -
+
+# For each regulon/operon, select a random set of genes that are the same size at the regulon/operon
+pa14_operon["Random_Genes"] = pa14_operon["Length_processed"].apply(
+    lambda num_genes: pa14_membership.sample(num_genes).index.values
+)
+pa14_regulon["Random_Genes"] = pa14_regulon["Length_processed"].apply(
+    lambda num_genes: pa14_membership.sample(num_genes).index.values
+)
+
+pao1_operon.head()
+
+pa14_operon.head()
+
 # ### Calculate the distribution
 
 # For each regulon/operon get the number of modules that regulon/operon genes are found in, number of modules
@@ -262,6 +346,15 @@ pao1_operon["Num_random_modules"] = pao1_operon["Random_Genes"].apply(
     lambda list_genes: pao1_membership.loc[list_genes]["module id"].nunique()
 )
 
+# For PA14
+pa14_operon["Num_operon_modules"] = pa14_operon["Genes_processed"].apply(
+    lambda list_genes: pa14_membership.loc[list_genes]["module id"].nunique()
+)
+pa14_operon["Num_random_modules"] = pa14_operon["Random_Genes"].apply(
+    lambda list_genes: pa14_membership.loc[list_genes]["module id"].nunique()
+)
+
+# For PAO1 regulons
 pao1_regulon["Num_regulon_modules"] = pao1_regulon["Genes_processed"].apply(
     lambda list_genes: pao1_membership.loc[list_genes]["module id"].nunique()
 )
@@ -269,9 +362,25 @@ pao1_regulon["Num_random_modules"] = pao1_regulon["Random_Genes"].apply(
     lambda list_genes: pao1_membership.loc[list_genes]["module id"].nunique()
 )
 
+# For PA14 regulons
+pa14_regulon["Num_regulon_modules"] = pa14_regulon["Genes_processed"].apply(
+    lambda list_genes: pa14_membership.loc[list_genes]["module id"].nunique()
+)
+pa14_regulon["Num_random_modules"] = pa14_regulon["Random_Genes"].apply(
+    lambda list_genes: pa14_membership.loc[list_genes]["module id"].nunique()
+)
+
+print(pao1_operon.shape)
 pao1_operon.head()
 
+print(pa14_operon.shape)
+pa14_operon.head()
+
+print(pao1_regulon.shape)
 pao1_regulon.head()
+
+print(pa14_regulon.shape)
+pa14_regulon.head()
 
 # +
 # Format df for plotting using displot
@@ -283,6 +392,16 @@ pao1_regulon_toplot = pd.melt(
 )
 
 pao1_operon_toplot.tail()
+
+# +
+pa14_operon_toplot = pd.melt(
+    pa14_operon, value_vars=["Num_operon_modules", "Num_random_modules"]
+)
+pa14_regulon_toplot = pd.melt(
+    pa14_regulon, value_vars=["Num_regulon_modules", "Num_random_modules"]
+)
+
+pa14_operon_toplot.tail()
 
 
 # -
@@ -325,7 +444,7 @@ cumulative_distribution(
     color="blue",
 )
 _ = plt.legend()
-plt.title("Cumulative distribution of module counts (operon vs random genes)")
+plt.title("Cumulative distribution of PAO1 module counts (operon vs random genes)")
 plt.ylabel("Number of operons/random groups")
 plt.xlabel("The number of modules that operons/random genes are contained in")
 
@@ -343,6 +462,26 @@ plt.title("PMF distribution of module counts (operon vs random genes)")
 
 # +
 cumulative_distribution(
+    pa14_operon["Num_operon_modules"],
+    label="Number of modules containing operon genes",
+    color="red",
+)
+cumulative_distribution(
+    pa14_operon["Num_random_modules"],
+    label="Number of modules containing random genes",
+    color="blue",
+)
+_ = plt.legend()
+plt.title("Cumulative distribution of PA14 module counts (operon vs random genes)")
+plt.ylabel("Number of operons/random groups")
+plt.xlabel("The number of modules that operons/random genes are contained in")
+
+scipy.stats.ks_2samp(
+    pa14_operon["Num_operon_modules"], pa14_operon["Num_random_modules"]
+)
+
+# +
+cumulative_distribution(
     pao1_regulon["Num_regulon_modules"],
     label="Number of modules containing regulon genes",
     color="red",
@@ -353,7 +492,7 @@ cumulative_distribution(
     color="blue",
 )
 _ = plt.legend()
-plt.title("Cumulative distribution of module counts (regulon vs random genes)")
+plt.title("Cumulative distribution of PAO1 module counts (regulon vs random genes)")
 plt.ylabel("Number of regulons/random groups")
 plt.xlabel("The number of modules that regulons/random genes are contained in")
 
@@ -368,6 +507,27 @@ fig = sns.displot(
     hue="variable",
 )
 plt.title("PMF distribution of module counts (regulon vs random genes)")
+
+# +
+cumulative_distribution(
+    pa14_regulon["Num_regulon_modules"],
+    label="Number of modules containing regulon genes",
+    color="red",
+)
+cumulative_distribution(
+    pa14_regulon["Num_random_modules"],
+    label="Number of modules containing random genes",
+    color="blue",
+)
+_ = plt.legend()
+plt.title("Cumulative distribution of PA14 module counts (regulon vs random genes)")
+plt.ylabel("Number of regulons/random groups")
+plt.xlabel("The number of modules that regulons/random genes are contained in")
+
+scipy.stats.ks_2samp(
+    pa14_regulon["Num_regulon_modules"], pa14_regulon["Num_random_modules"]
+)
+# -
 
 # ### Check what size the modules are that regulon/operon/random genes are found in
 
@@ -394,9 +554,9 @@ pao1_regulon.head()
 #     * y-axis = The number of operon/regulon (red) or random (blue) groups.
 #     * x-axis = The number of modules that operon/regulon/random genes are contained in
 #
-# * Looking at the operon plot, the value at "1" on the x-axis says that there are 75 operons found in exactly 1 module, and there are 10 random groups found in exactly 1 module
-#     * Then the increase at "2" on the x-axis is the number of operons or random genes that are spread across 2 or 1 different modules (this is the cumulative part). In other words, a random set (size matched with the operon) where the genes in that set are found in 2 modules.
-#     * There are 150 operons that are found in either 1 or 2 modules. There are 120 random groups that are found in either 1 or 2 modules.
+# * Looking at the regulon plot, the value at "1" on the x-axis says that there is 1 regulon found in exactly 1 module, and there are 0 random groups found in exactly 1 module
+#     * Then the increase at "2" on the x-axis is the number of regulons or random genes that are spread across 2 or 1 different modules (this is the cumulative part). In other words, a random set (size matched with the operon) where the genes in that set are found in 2 modules.
+#     * There are 3 regulons that are found in either 1 or 2 modules. There are 2 random groups that are found in either 1 or 2 modules.
 #     * Then if you compare the blue and the red curves, the vertical distance between the two curves tells you how much of a shift there is between the distributions.
 # * These distribution plots are summing counts as you move from left to right, so a shift in the curves corresponds to a shift in the distribution (i.e. a curve shifted to the right means that the distribution is shifted to the right)
 #
@@ -404,4 +564,4 @@ pao1_regulon.head()
 # * We can perform [Kolmogorov-Smirnov test](https://en.wikipedia.org/wiki/Kolmogorov%E2%80%93Smirnov_test) to compare the distribution of module counts for genes in regulons/operons versus random genes. The KS test will quantify the difference in the cumulative distribution curves.
 #
 # * Based on the KS test, there is a significant difference (across thresholds) between the operon and random distribution, as we would expect.
-#     * There is only a significant difference between the regulon and random distributions at lower thresholds (0.7 and below). The lack of significance at higher thresholds is likely due to the small sample size (i.e. 6 or 10 regulons have genes that are contained in modules with fewer than 1000 genes)
+#     * There is only a significant difference between the regulon and random distributions. The lack of significance is likely due to the small sample size (i.e. 6 or 10 regulons have genes that are contained in modules with fewer than 1000 genes)
