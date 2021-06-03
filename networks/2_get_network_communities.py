0996cf12b6f1833ea9b4ae30efdd605610fd0914
Alexandra Lee <alexjlee.21@gmail.com>
2021-06-03 10:20:06 -0400

    Update clustering scripts (#27)
    * update clustering scripts
    
    * update notebook based on PR

--- networks/2_get_network_communities.py
+++ networks/2_get_network_communities.py
@@ -35,20 +35,25 @@ from core_acc_modules import paths
 # We will run this notebook for each threshold parameter
 
 # +
-# Params
-corr_threshold = 0.5
+# User params to set
+
+# Clustering method
+# Choices: {"dbscan", "hierarchal", "affinity"}
+cluster_method = "affinity"
+
+# DBSCAN params
+density_threshold = 8
+
+# Hierarchical clustering params
+hier_threshold = 8
+link_dist = "average"
+
+# Affinity params
+affinity_damping = 0.6
 
 # Correlation matrix files
-pao1_corr_filename = os.path.join(
-    paths.LOCAL_DATA_DIR, f"pao1_corr_{corr_threshold}.tsv"
-)
-pa14_corr_filename = os.path.join(
-    paths.LOCAL_DATA_DIR, f"pa14_corr_{corr_threshold}.tsv"
-)
-
-# +
-## TO DO
-# Create variable to determine which clustering method to use
+pao1_corr_filename = paths.PAO1_CORR_LOG_SPELL
+pa14_corr_filename = paths.PA14_CORR_LOG_SPELL
 # -
 
 # Load correlation data
@@ -64,40 +69,46 @@ pa14_corr = pd.read_csv(pa14_corr_filename, sep="\t", index_col=0, header=0)
 # A cluster is a set of core samples that can be built by recursively taking a core sample, finding all of its neighbors that are core samples, finding all of their neighbors that are core samples, and so on. A cluster also has a set of non-core samples, which are samples that are neighbors of a core sample in the cluster but are not themselves core samples. Intuitively, these samples are on the fringes of a cluster.
 #
 # * We define a core sample as being a sample in the dataset such that there exist `min_samples` other samples within a distance of `eps`, which are defined as neighbors of the core sample.
-# * Using all default values
+# * Here we use `eps=8` based on the observations in the [prevous notebook](1_correlation_analysis.ipynb). In the previous notebook we plotted the distribution of pairwise distances (pdist) per gene and we selected 8 based on where the distribution curve drops off on the left side to mark how similar gene pairs are.
 #
 
-# +
 # Clustering using DBSCAN
-# pao1_clustering = DBSCAN().fit(pao1_corr)
-# pa14_clustering = DBSCAN().fit(pa14_corr)
-# -
+if cluster_method == "dbscan":
+    pao1_clustering = DBSCAN(eps=density_threshold).fit(pao1_corr)
+    pa14_clustering = DBSCAN(eps=density_threshold).fit(pa14_corr)
 
 # ### Hierarchical clustering
 # [Hierarchical clustering](https://scikit-learn.org/stable/modules/generated/sklearn.cluster.AgglomerativeClustering.html#sklearn.cluster.AgglomerativeClustering): Initially, each object is assigned to its own cluster and then the algorithm proceeds iteratively, at each stage joining the two most similar clusters (i.e. linkage distance is minimized), continuing until there is just a single cluster.
 #
 # * n_cluster: The number of clusters to find.
 # * linkage: Criterion used to determine distance between observations. 'average'=average distance of each observation in the two sets.
+# * distance_threshold: The linkage distance threshold above which, clusters will not be merged
+# * Here we use `distance_threshold=8` based on the observations in the [prevous notebook](1_correlation_analysis.ipynb). In the previous notebook we plotted the distribution of pairwise distances (pdist) per gene and we selected 8 based on where the distribution curve drops off on the left side to mark how similar gene pairs are.
 #
 # * Note: It looks like this method tends to produce 1 very large cluster. To break this up we will iteratively apply hierarchal clustering on the largest cluster.
 
 # Clustering using hierarchal clustering
-pao1_clustering = AgglomerativeClustering(
-    n_clusters=None, distance_threshold=0.5, linkage="average"
-).fit(pao1_corr)
-pa14_clustering = AgglomerativeClustering(
-    n_clusters=None, distance_threshold=0.5, linkage="average"
-).fit(pa14_corr)
+if cluster_method == "hierarchal":
+    pao1_clustering = AgglomerativeClustering(
+        n_clusters=None, distance_threshold=hier_threshold, linkage=link_dist
+    ).fit(pao1_corr)
+    pa14_clustering = AgglomerativeClustering(
+        n_clusters=None, distance_threshold=hier_threshold, linkage=link_dist
+    ).fit(pa14_corr)
 
 # ### Affinity propogation
 #
-# [Affinity propogation](https://scikit-learn.org/stable/modules/generated/sklearn.cluster.AffinityPropagation.html#sklearn.cluster.AffinityPropagation):
+# [Affinity propogation](https://scikit-learn.org/stable/modules/generated/sklearn.cluster.AffinityPropagation.html#sklearn.cluster.AffinityPropagation): creates clusters by sending messages between pairs of samples until convergence. The messages sent between points belong to one of two categories. The first is the responsibility $r(k,i)$, which is the accumulated evidence that sample $k$ should be the exemplar for sample $i$ compared to other exemplars. The second is the availability $a(k,i)$ which is the accumulated evidence that sample $i$ should choose sample $k$to be its exemplar. _Exemplar_ meaning the members of the input set that are representative of clusters -- similar to _centroids_ in k-means. Unlike k-means this method doesn't require a preset $k$ to be chosen.
+#
+# * damping: Damping factor (between 0.5 and 1) is the extent to which the current value is maintained relative to incoming values (weighted 1 - damping). This in order to avoid numerical oscillations when updating these values. Default is 0.5. Using default for PA14 data, the model didn't converge so we increased this to 0.6.
+#
 
-# +
 # Clustering using affinity propogation
-# pao1_clustering = AffinityPropagation().fit(pao1_corr)
-# pa14_clustering = AffinityPropagation().fit(pa14_corr)
-# -
+if cluster_method == "affinity":
+    pao1_clustering = AffinityPropagation(random_state=0).fit(pao1_corr)
+    pa14_clustering = AffinityPropagation(random_state=0, damping=affinity_damping).fit(
+        pa14_corr
+    )
 
 # ## Membership assignments
 
@@ -125,7 +136,12 @@ pa14_membership_df["module id"].value_counts()
 #
 #     Thoughts on different methods
 
-# +
 # Save membership dataframe
-# pao1_membership_df.to_csv(pao1_membership_filename, sep="\t")
-# pa14_membership_df.to_csv(pa14_membership_filename, sep="\t")
+pao1_membership_filename = os.path.join(
+    paths.LOCAL_DATA_DIR, f"pao1_modules_{cluster_method}.tsv"
+)
+pa14_membership_filename = os.path.join(
+    paths.LOCAL_DATA_DIR, f"pa14_modules_{cluster_method}.tsv"
+)
+pao1_membership_df.to_csv(pao1_membership_filename, sep="\t")
+pa14_membership_df.to_csv(pa14_membership_filename, sep="\t")
