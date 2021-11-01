cc9a33c610011a1df1b0ee3d78aa148e06b570bf
ajlee21 <alexjlee.21@gmail.com>
2021-11-01 15:47:33 -0400

    rerun corr after updating data partitioning and num SVs to use

--- 2_correlation_analysis/1_correlation_analysis.py
+++ 2_correlation_analysis/1_correlation_analysis.py
@@ -43,10 +43,14 @@ from scripts import paths, utils
 
 # +
 # Params
-num_singular_values_log = 100
 
 # Which subset of genes to consider: core, acc, all
-subset_genes = "core"
+subset_genes = "all"
+
+if subset_genes == "acc":
+    num_SVs = 50
+else:
+    num_SVs = 100
 # -
 
 # Load expression data
@@ -90,6 +94,19 @@ pa14_core = core_acc_dict["core_pa14"]
 pao1_acc = core_acc_dict["acc_pao1"]
 pa14_acc = core_acc_dict["acc_pa14"]
 
+# ## Select subset of genes
+
+# Select subset of genes
+if subset_genes == "core":
+    pao1_compendium = pao1_compendium[pao1_core]
+    pa14_compendium = pa14_compendium[pa14_core]
+elif subset_genes == "acc":
+    pao1_compendium = pao1_compendium[pao1_acc]
+    pa14_compendium = pa14_compendium[pa14_acc]
+
+print(pao1_compendium.shape)
+print(pa14_compendium.shape)
+
 # ## Log transform + SPELL Correlation
 #
 # _Review of SVD_
@@ -127,17 +144,8 @@ pa14_U_df = pd.DataFrame(data=pa14_U, index=pa14_compendium_T.index)
 
 # Correlation of U
 # Since `corr()` computes pairwise correlation of columns we need to invert U
-pao1_corr_log_spell = pao1_U_df.iloc[:, :num_singular_values_log].T.corr()
-pa14_corr_log_spell = pa14_U_df.iloc[:, :num_singular_values_log].T.corr()
-
-# Select subset of genes
-# Note: genes are selected after correlation is performed
-if subset_genes == "core":
-    pao1_corr_log_spell = pao1_corr_log_spell.loc[pao1_core, pao1_core]
-    pa14_corr_log_spell = pa14_corr_log_spell.loc[pa14_core, pa14_core]
-elif subset_genes == "acc":
-    pao1_corr_log_spell = pao1_corr_log_spell.loc[pao1_acc, pao1_acc]
-    pa14_corr_log_spell = pa14_corr_log_spell.loc[pa14_acc, pa14_acc]
+pao1_corr_log_spell = pao1_U_df.iloc[:, :num_SVs].T.corr()
+pa14_corr_log_spell = pa14_U_df.iloc[:, :num_SVs].T.corr()
 
 # Check for duplicates indices
 assert pao1_corr_log_spell.index.duplicated().sum() == 0
@@ -169,10 +177,11 @@ sns.displot(flat_pa14_corr_df["corr_val"])
 
 # +
 # Plot heatmap
-h1a = sns.clustermap(pao1_corr_log_spell, cmap="icefire", figsize=(20, 20))
+h1a = sns.clustermap(pao1_corr_log_spell, cmap="BrBG", center=0, figsize=(20, 20))
 h1a.fig.suptitle(
-    f"log transform + SPELL corrected using {num_singular_values_log} vectors (PAO1)",
+    f"log transform + SPELL corrected using {num_SVs} vectors (PAO1)",
     y=1.05,
+    fontsize=24,
 )
 
 # Save
@@ -182,10 +191,11 @@ pao1_log_spell_filename = os.path.join(
 h1a.savefig(pao1_log_spell_filename, dpi=300)
 
 # +
-h2a = sns.clustermap(pa14_corr_log_spell, cmap="icefire", figsize=(20, 20))
+h2a = sns.clustermap(pa14_corr_log_spell, cmap="BrBG", center=0, figsize=(20, 20))
 h2a.fig.suptitle(
-    f"log transformed + SPELL corrected using {num_singular_values_log} vectors (PA14)",
+    f"log transformed + SPELL corrected using {num_SVs} vectors (PA14)",
     y=1.05,
+    fontsize=24,
 )
 
 # Save
